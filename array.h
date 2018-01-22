99fc654470699b6ff3bda93fa39df218e44b65e1
Oliver Faircliff <mail@oliverfaircliff.com>
2018-01-22 17:16:49 +0000

    Add dynamic element size to array.h

--- array.h
+++ array.h
@@ -38,7 +38,6 @@
 
 
 #define ARRAY_ALLOCATE _ARRAY_ALLOCATE_FUNC
-#define ARRAY_ALLOCATE_TYPES(T, n) (_ARRAY_ALLOCATE_FUNC(sizeof(T) * (n)))
 #define ARRAY_ALLOCATE_SIZE(size, n) (_ARRAY_ALLOCATE_FUNC((size)  * (n)))
 #define ARRAY_FREE(p) (_ARRAY_FREE_FUNC(p))
 #define ARRAY_ASSERT(exp) (_ARRAY_ASSERT_FUNC(exp))
@@ -48,78 +47,34 @@
 
 namespace Array
 {
-  template <typename T>
-  struct Array;
-
-  // Array procedure prototypes
-  // All take an Array<T> reference as their first argument
-  //
-
-  template <typename T>
-  T *
-  get(Array<T>& array, u32 index);
-
-  template <typename T>
-  s32
-  get_element_index(Array<T>& array, T& element);
-
-  template <typename T>
-  void
-  add_n(Array<T>& array, T *new_elements, u32 n_new_elements);
-
-  template <typename T>
-  void
-  add(Array<T>& array, Array<T>& new_elements);
-
-  template <typename T>
-  void
-  add(Array<T>& array, T new_element);
-
-  template <typename T>
-  void
-  remove(Array<T>& array, u32 index);
-
-  template <typename T>
-  void
-  remove_ordered(Array<T>& array, u32 remove_at_index);
-
-  template <typename T>
-  void
-  free_array(Array<T>& array);
-
-  template <typename T>
-  void
-  clear(Array<T>& array);
-
-  template <typename T>
-  void
-  resize(Array<T>& array, u32 new_array_size);
-
-
   // Array<T> struct
   //
   // Array is a templated dynamic continuous array allocated on the heap.  The struct
   // includes an integral array so initial/small array(s) can be used without heap allocation.
   //
-  template <typename T>
+  template <typename T, bool dynamic_elem_size=false>
   struct Array
   {
     u32 array_size;  // Size of array pointed to by elements (in T units)
     u32 n_elements;  // Number of Ts in use in elements
+
+    // The number of bytes used per element, used instead of sizeof(T) for dynamic struct sizes.
+    //   Set to the default of sizeof(T) in resize or get (the only places it is used) if equal to
+    //   zero.
+    u32 element_size;
 
     // Static storage for small number of elements, prevents heap allocation for small arrays
     T static_elements[ARRAY_DEFAULT_INITIAL_SIZE];
 
     T *elements;  // Pointer to array memory
 
-    // Constructor:  Needed to test for - and do - initial heap allocation
-    Array(u32 initial_size = ARRAY_DEFAULT_INITIAL_SIZE)
+    // Constructor to zero members
+    Array(u32 initial_size = ARRAY_DEFAULT_INITIAL_SIZE, u32 _element_size = sizeof(T))
       : array_size(0),
         n_elements(0),
+        element_size(_element_size),
         elements(0)
-    {
-      resize(*this, initial_size);
-    }
+    {}
 
     // De-constructor: Free heap allocation
     ~Array()
@@ -136,18 +91,25 @@ namespace Array
   };
 
 
-  // Resize array memory to new_array_size*sizeof(T) and copy elements into the new memory.
+  // Resize array memory to new_array_size*element_size and copy elements into the new memory.
   //
   // If new_array_size < n_elements, the array is truncated to the maximum number of elements that
   //   will fit in the new array.
   //
-  // If new_array_size <= ARRAY_DEFAULT_INITIAL_SIZE, the static_elements array in Array is
-  //   used instead to avoid heap allocations, for small short-lived arrays.
+  // If new_array_size <= ARRAY_DEFAULT_INITIAL_SIZE, and element_size is not dynamic (!=sizeof(T)),
+  //   the static_elements array in Array is used instead to avoid heap allocations, for small
+  //   short-lived arrays.
   //
-  template <typename T>
+  template <typename T, bool dynamic_elem_size>
   void
-  resize(Array<T>& array, u32 new_array_size)
+  resize(Array<T, dynamic_elem_size>& array, u32 new_array_size)
   {
+    // Ensure element_size is initialised
+    if (array.element_size == 0)
+    {
+      array.element_size = sizeof(T);
+    }
+
     if (new_array_size == array.n_elements)
     {
       // No change
@@ -155,16 +117,18 @@ namespace Array
     else
     {
       T *new_buffer = 0;
-      if (new_array_size <= ARRAY_DEFAULT_INITIAL_SIZE)
+
+      if (!dynamic_elem_size &&
+          new_array_size <= ARRAY_DEFAULT_INITIAL_SIZE)
       {
-        // Use static struct array
+        // Use static struct array if element_size isn't dynamic, and the new size fits in static
         new_buffer = array.static_elements;
         new_array_size = ARRAY_DEFAULT_INITIAL_SIZE;
       }
       else
       {
         // Allocate array on heap
-        new_buffer = (T *)ARRAY_ALLOCATE_TYPES(T, new_array_size);
+        new_buffer = (T *)ARRAY_ALLOCATE_SIZE(array.element_size, new_array_size);
       }
 
       ARRAY_ASSERT(new_buffer != 0);
@@ -183,7 +147,7 @@ namespace Array
       // Don't copy if there wasn't a buffer previously allocated
       if (array.elements != 0)
       {
-        ARRAY_MEMCPY(new_buffer, array.elements, array.n_elements*sizeof(T));
+        ARRAY_MEMCPY(new_buffer, array.elements, array.n_elements * array.element_size);
       }
 
       // Free the old elements buffer, unless we were using the static_element buffer
@@ -199,23 +163,84 @@ namespace Array
   }
 
 
+  template<typename T, bool dynamic_elem_size>
+  void
+  expand_array(Array<T, dynamic_elem_size>& array, u32 space_for_n_elements)
+  {
+    u32 new_smallest_size = array.array_size + space_for_n_elements;
+
+    u32 new_size;
+    if (array.array_size == 0)
+    {
+      new_size = ARRAY_DEFAULT_INITIAL_SIZE;
+    }
+    else
+    {
+      new_size = array.array_size * 2;
+    }
+
+    while (new_size < new_smallest_size)
+    {
+      new_size *= 2;
+    }
+
+    resize(array, new_size);
+  }
+
+
+  // Unallocates the memory used by the array, and zeros n_elements
+  //
+  template<typename T, bool dynamic_elem_size>
+  void
+  free_array(Array<T, dynamic_elem_size>& array)
+  {
+    if (array.elements != array.static_elements &&
+        array.elements != 0)
+    {
+      ARRAY_FREE(array.elements);
+      array.array_size = 0;
+    }
+    array.elements = 0;
+    array.n_elements = 0;
+  }
+
+
+  // Clears references to all elements in the array, but doesn't free the memory.  Useful if you need
+  //   to delete all elements, but are likely to reuse the array and want to save the reallocation.
+  //
+  template<typename T, bool dynamic_elem_size>
+  void
+  clear(Array<T, dynamic_elem_size>& array)
+  {
+    array.n_elements = 0;
+  }
+
+
   //
   // Getters
   //
 
-  template <typename T>
+  template<typename T, bool dynamic_elem_size>
   T *
-  get(Array<T>& array, const u32 index)
+  get(Array<T, dynamic_elem_size>& array, const u32 index)
   {
     ARRAY_ASSERT(index < array.n_elements);
-    return array.elements + index;
+
+    // Ensure element_size is initialised
+    if (array.element_size == 0)
+    {
+      array.element_size = sizeof(T);
+    }
+
+    // Have to do indexing in bytes to deal with dynamic element size
+    return (T *)((u8 *)array.elements + (index * array.element_size));
   }
 
   // Searches the array for the index of the first element equal to the given element
   //
-  template <typename T>
+  template<typename T, bool dynamic_elem_size>
   s32
-  get_element_index(Array<T>& array, T& element)
+  get_element_index(Array<T, dynamic_elem_size>& array, T& element)
   {
     s32 result = -1;
 
@@ -236,65 +261,74 @@ namespace Array
   }
 
 
-  // Unallocates the memory used by the array, and zeros n_elements
-  //
-  template <typename T>
-  void
-  free_array(Array<T>& array)
-  {
-    if (array.elements != array.static_elements &&
-        array.elements != 0)
-    {
-      ARRAY_FREE(array.elements);
-      array.array_size = 0;
-    }
-    array.elements = 0;
-    array.n_elements = 0;
-  }
-
-
-  // Clears references to all elements in the array, but doesn't free the memory.  Useful if you need
-  //   to delete all elements, but are likely to reuse the array and want to save the reallocation.
-  //
-  template <typename T>
-  void
-  clear(Array<T>& array)
-  {
-    array.n_elements = 0;
-  }
-
-
   //
   // Element Adders
   //
 
-  template <typename T>
-  void
-  add_n(Array<T>& array, T *new_elements, u32 n_new_elements)
+  template<typename T, bool dynamic_elem_size>
+  T *
+  add_n(Array<T, dynamic_elem_size>& array, u32 n_new_elements)
   {
     u32 space_left = array.array_size - array.n_elements;
     if (space_left < n_new_elements)
     {
-      resize(array, array.array_size * 2);
+      expand_array(array, n_new_elements);
     }
 
-    T *add_position = array.elements + array.n_elements;
-    ARRAY_MEMCPY(add_position, new_elements, n_new_elements*sizeof(T));
+    u32 new_elements_start_index = array.n_elements;
+
     array.n_elements += n_new_elements;
+    T *new_elements_start = get(array, new_elements_start_index);
+
+    return new_elements_start;
   }
 
-  template <typename T>
+  template<typename T, bool dynamic_elem_size>
   void
-  add(Array<T>& array, Array<T>& new_elements)
+  add_n(Array<T, dynamic_elem_size>& array, T *new_elements, u32 n_new_elements)
   {
-    add_n(array, new_elements.elements, new_elements.n_elements);
+    T *add_position = add_n(array, n_new_elements);
+    ARRAY_MEMCPY(add_position, new_elements, n_new_elements*array.element_size);
   }
 
-  template <typename T>
+  // This procedure adds all the elements from another (different) array with the same element type
+  //
+  template<typename T, bool dynamic_elem_size>
   void
-  add(Array<T>& array, T new_element)
+  add(Array<T, dynamic_elem_size>& array, Array<T, dynamic_elem_size>& other_array)
+  {
+    add_n(array, other_array.elements, other_array.n_elements);
+  }
+
+  // Create new element, return pointer
+  template<typename T, bool dynamic_elem_size>
+  T *
+  add(Array<T, dynamic_elem_size>& array)
+  {
+    return add_n(array, 1);
+  }
+
+  // Create new element, return reference (enables: Array::new_element(arr) = item; )
+  template<typename T, bool dynamic_elem_size>
+  T &
+  new_element(Array<T, dynamic_elem_size>& array)
+  {
+    T *new_element = add_n(array, 1);
+    return (*new_element);
+  }
+
+  template<typename T, bool dynamic_elem_size>
+  void
+  add(Array<T, dynamic_elem_size>& array, T new_element)
   {
     add_n(array, &new_element, 1);
+  }
+
+  template<typename T, bool dynamic_elem_size>
+  void
+  add(Array<T, dynamic_elem_size>& array, T *new_element)
+  {
+    add_n(array, new_element, 1);
   }
 
 
@@ -304,9 +338,9 @@ namespace Array
 
   // Unordered remove
   // Last element is moved into deleted element's position
-  template <typename T>
+  template<typename T, bool dynamic_elem_size>
   void
-  remove(Array<T>& array, u32 index)
+  remove(Array<T, dynamic_elem_size>& array, u32 index)
   {
     // Must get pointers before decrementing n_elements, so assertion in getter is not triggered
     T *to_remove = get(array, index);
@@ -322,9 +356,9 @@ namespace Array
 
   // Ordered remove
   // All elements beyond deleted item are shifted up one place
-  template <typename T>
+  template<typename T, bool dynamic_elem_size>
   void
-  remove_ordered(Array<T>& array, u32 remove_at_index)
+  remove_ordered(Array<T, dynamic_elem_size>& array, u32 remove_at_index)
   {
     for (u32 index = remove_at_index;
          index < array.n_elements - 1;
